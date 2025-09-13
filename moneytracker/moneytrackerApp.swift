1cc013a69efbecae7c6ded03184df08f997bff05
FlyingAgent <joostgroen08@gmail.com>
2025-09-13 14:34:38 +0200

    Initial Commit

---
+++ moneytracker/moneytrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneytrackerApp.swift
+//  moneytracker
+//
+//  Created by Joost Groen on 13.09.25.
+//
+
+import SwiftUI
+
+@main
+struct moneytrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moneytrackerTests/moneytrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  moneytrackerTests.swift
+//  moneytrackerTests
+//
+//  Created by Joost Groen on 13.09.25.
+//
+
+import Testing
+@testable import moneytracker
+
+struct moneytrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
